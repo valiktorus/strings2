cd5be91c6469c2d174be4700bf8c826969089487
valiktorus <valiktorus@mail.ru>
2017-04-15 20:00:14 +0300

    1

---
+++ src/Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+    public static final String PROPERTY_FILE = "in1";
+    public static final String LINES_STARTS_WITH_INDEX = "index.*";
+    public static final String CORRECT_INDEX_LINE = "^index([1-9]\\d*)$";
+    public static final String CORRECT_INDEX_VALUE = "^[1-9]\\d*$";
+    public static final String VALUE = "value";
+    public static final String OUTPUT_LINE = "sum = %s%nerror-lines = %d";
+}
--- src/Runner.java
+++ src/Runner.java
@@ -1,37 +1,42 @@
 import java.util.Enumeration;
 import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Runner {
     public static void main(String[] args) {
-        ResourceBundle rb = ResourceBundle.getBundle("in1", Locale.ENGLISH);
+        ResourceBundle rb = ResourceBundle.getBundle(Constants.PROPERTY_FILE, Locale.ENGLISH);
         Enumeration<String> keys = rb.getKeys();
         double sum = 0;
         int errorLines = 0;
+        String indexValue;
         String key;
-        String keyValue;
-        String indexNumber = null;
         String requiredValue;
         while (keys.hasMoreElements()){
-            key = keys.nextElement();
-            keyValue = rb.getString(key);
-            Pattern pattern = Pattern.compile("^index(\\d+)$");
-            Matcher matcher = pattern.matcher(key);
-            if (matcher.find()){
-                indexNumber = matcher.group(1);
-                try {
-                    requiredValue = rb.getString("value" + indexNumber + keyValue).trim();
-                    sum += Double.parseDouble(requiredValue);
-                }catch (Exception e){
-                    errorLines++;
+            try{
+                key = keys.nextElement();
+                if (key.matches(Constants.LINES_STARTS_WITH_INDEX)){
+                    Pattern pattern = Pattern.compile(Constants.CORRECT_INDEX_LINE);
+                    Matcher matcher = pattern.matcher(key);
+                    if (matcher.find()){
+                        indexValue = rb.getString(key);
+                        if (indexValue.matches(Constants.CORRECT_INDEX_VALUE)){
+                            requiredValue = rb.getString(Constants.VALUE + matcher.group(1) + indexValue);
+                                sum += Double.parseDouble(requiredValue);
+                        }else {
+                            errorLines++;
+                        }
+                    }else {
+                        errorLines++;
+                    }
                 }
+            }catch (MissingResourceException|NumberFormatException e){
+                errorLines++;
             }
-
         }
-        System.out.println(sum);
-        System.out.println(errorLines);
+        System.out.printf(Constants.OUTPUT_LINE, sum , errorLines);
 
     }
 }
